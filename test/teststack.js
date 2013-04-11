ffc4ac2dd86b43785dc9599ca3a10bcc67cdd478
Matthew Maxwell <mmaxwell@sitepen.com>
2013-04-10 21:16:46 -0500

    refs #24224; Changed paths back.

--- test/teststack.js
+++ test/teststack.js
@@ -46,11 +46,11 @@ define({
 	loader: {
 		// Packages that should be registered with the loader in each testing environment
 		packages: [ 'dojo2-teststack' ],
-		map: { 'dojo2-teststack': { 'dojo-ts': './dojo' } }
+		map: { 'dojo2-teststack': { 'dojo-ts': 'dojo2-teststack/dojo' } }
 	},
 
 	// Non-functional test suite(s) to run in each browser
-	suites: [ './test/all' ],
+	suites: [ 'dojo2-teststack/test/all' ],
 
 	// Functional test suite(s) to run in each browser once non-functional tests are completed
 	functionalSuites: [ ],
